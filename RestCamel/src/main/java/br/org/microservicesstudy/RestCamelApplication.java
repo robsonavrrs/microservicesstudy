e552f76903818f5ffb5f089ba023a6672cccb476
robson <robson@monstro>
2017-03-25 14:29:34 -0300

    Versão inicial do projeto.

---
+++ RestCamel/src/main/java/br/org/microservicesstudy/RestCamelApplication.java
@@ -0,0 +1,12 @@
+package br.org.microservicesstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCamelApplication.class, args);
+	}
+}
