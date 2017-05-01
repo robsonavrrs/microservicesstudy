0651ea61b1da82dad150d258ddae3b6de473d00e
chocomachine <chocomachine@DESKTOP-L1JB09T>
2017-05-01 20:47:43 -0300

    Inclusão do banco h2, mapeamento de tabela e criação da estrutura inicial de crud.

--- demoresource/src/main/java/com/example/peopleresource/domain/people/People.java
+++ demoresource/src/main/java/com/example/peopleresource/domain/people/People.java
@@ -1,16 +1,43 @@
 package com.example.peopleresource.domain.people;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="PEOPLE")
+@Builder
 public class People {
 
 	
-	private String nome;
-
-	public String getNome() {
-		return nome;
+	public People(){
+		
 	}
-
-	public void setNome(String nome) {
+	
+	public People(Long id, String nome){
+		this.id = id;
 		this.nome = nome;
 	}
+	
+	@Id
+	@SequenceGenerator(name = "idGen", sequenceName = "SQ_PEOPLE")
+	@GeneratedValue(generator = "idGen")
+	@Column(name="ID")
+	@Getter
+	@Setter
+	private Long id;
+	
+	@Column(name="NAME")
+	@Getter
+	@Setter
+	private String nome;
+
 	
 }
--- demoresource/src/main/java/com/example/peopleresource/interfaces/rest/PeopleResource.java
+++ demoresource/src/main/java/com/example/peopleresource/interfaces/rest/PeopleResource.java
@@ -1,23 +1,41 @@
 package com.example.peopleresource.interfaces.rest;
 
-import com.example.peopleresource.domain.people.People;
-import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.collections.IteratorUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.peopleresource.application.PeopleService;
+import com.example.peopleresource.domain.people.People;
+import com.example.peopleresource.domain.people.PeopleRepository;
 
 @RestController
 @RequestMapping(path="/v1.0.0/peoples")
 public class PeopleResource {
 	
-	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	@Autowired
+	private PeopleRepository peopleRepository;
+	
+	@Autowired
+	private PeopleService peopleService;
+	
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<People> getPeoples(){
-		People people = new People();
-		people.setNome("teste People");
-		
-		return Arrays.asList(people);
+	
+		return IteratorUtils.toList(peopleRepository.findAll().iterator());
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public People postPeoples(@RequestBody People newPeople){
+		return peopleService.createPeople(newPeople);
 	}
 
 }
